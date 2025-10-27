f353d4087292db292f641daddb2bc8d81737b450
dominiks <dominiksvecbastiks@gmail.com>
2025-10-27 11:08:14 +0200

    added file

---
+++ `ticket-price.js
@@ -0,0 +1,14 @@
+let visitorAge = 12;
+let isStudent = Boolean(true)
+
+
+if (visitorAge<=6) {
+    result= "ieeja bez maksas"
+}
+
+if (isStudent = true) {
+    result = "jāmaksā 5 eiro"
+}else{
+    result= "jāmaksā 10 eiro"
+}
+console.log(result)
